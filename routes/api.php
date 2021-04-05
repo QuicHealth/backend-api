c4afafba038258fce71ba9195dcd1613974ef7cd
ilelaboye <ilelaboyealekan@gmail.com>
2021-04-05 22:35:13 +0100

    controller setting

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function (){
+
+    Route::namespace('Patient')->group(function(){
+        //pascal api code goes here
+    });
+
+
+    Route::namespace('Hospital')->group(function(){
+        //abass api code goes here
+    });
+});
