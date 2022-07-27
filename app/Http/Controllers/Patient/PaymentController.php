8489347867d695bec6a27491df9f35d68e6b4886
officialozioma <dikep15@gmail.com>
2022-07-27 20:32:56 +0100

    new changes

--- app/Http/Controllers/Patient/PaymentController.php
+++ app/Http/Controllers/Patient/PaymentController.php
@@ -64,16 +64,17 @@ class PaymentController extends Controller
 
     public function payment_status(Request $request)
     {
+        dd($request->query('paymentReference'));
 
-        $transaction = Payment::where('paymentReference', $request->query('paymentReference'))->first();
+        // $transaction = Payment::where('paymentReference', $request->query('paymentReference'))->first();
 
-        if ($transaction) {
-            return UpdatePaymentAction::run($transaction);
-        } else {
-            return response([
-                'status' => "error",
-            ], 500);
-        }
+        // if ($transaction) {
+        //     return UpdatePaymentAction::run($transaction);
+        // } else {
+        //     return response([
+        //         'status' => "error",
+        //     ], 500);
+        // }
     }
 
 
