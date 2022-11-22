44980ddcc37659a846d80dc68b9b1cee92852db8
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2022-11-22 09:14:59 +0100

    created complain pages

---
+++ resources/views/admins/complains.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+<div class="page-content">
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                    <h4 class="mb-sm-0">Complains</h4>
+
+                    <div class="page-title-right">
+                        <ol class="breadcrumb m-0">
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">Message Managment</a></li>
+                            <li class="breadcrumb-item active">Complains</li>
+                        </ol>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card" id="leadsList">
+                    <div class="card-header border-0">
+
+                        <div class="row g-4 align-items-center">
+                            <div class="col-sm-3">
+                                <div class="search-box">
+                                    <input type="text" class="form-control search" placeholder="Search for...">
+                                    <i class="ri-search-line search-icon"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+</div>
+
+@endsection
