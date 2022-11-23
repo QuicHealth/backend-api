ce39227defca5e310187e6f995165a99e2a6dd58
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2022-11-23 10:23:06 +0100

    updated complains

--- resources/views/admins/complains.blade.php
+++ resources/views/admins/complains.blade.php
@@ -33,11 +33,92 @@
                                     <i class="ri-search-line search-icon"></i>
                                 </div>
                             </div>
+                            <div class="col-sm-auto ms-auto">
+                                <div class="hstack gap-2">
+                                    {{-- <a href="" class="btn btn-success add-btn"><i class="ri-add-line align-bottom me-1"></i> Add Donation</a> --}}
+                                </div>
+                            </div>
                         </div>
                     </div>
+                    <div class="card-body">
+                        <div>
+                            <div class="table-responsive table-card">
+                                <table class="table align-middle" id="customerTable">
+                                    <thead class="table-light">
+                                        <tr>
+                                            <th scope="col" style="width: 50px;">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" id="checkAll" value="option">
+                                                </div>
+                                            </th>
 
+                                            <th class="sort" data-sort="name">No</th>
+                                            <th class="sort" data-sort="name">User's Name</th>
+                                            <th class="sort" data-sort="leads_score">User's Email</th>
+                                            <th class="sort" data-sort="phone"scope="col">Complains</th>
+                                            <th class="sort" data-sort="date" scope="col">Date</th>
+                                            <th class="sort" data-sort="date">Action</th>
 
+                                        </tr>
+                                    </thead>
+                                    <tbody class="list form-check-all">
+                                        <tr >
+                                            <th scope="row">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" name="checkAll" value="option1">
+                                                </div>
+                                            </th>
+                                            <td class="id">1</td>
+                                            <td class="name">Mike</td>
+                                            <td class="name">mike@gmail.com</td>
+                                            <td class="name">I need help</td>
+                                            <td class="name">12/12/2022</td>
+                                            <td>
+                                                <ul class="list-inline hstack gap-2 mb-0">
+                                                    <li class="list-inline-item edit" data-bs-toggle="tooltip"
+                                                        data-bs-trigger="hover" data-bs-placement="top" title="Edit">
+                                                        <a href=""
+                                                            class="text-primary d-inline-block edit-item-btn">
+                                                            <i class="ri-pencil-fill fs-16"></i>
+                                                        </a>
+                                                    </li>
+                                                    <li class="list-inline-item remove" data-bs-toggle="tooltip"
+                                                        data-bs-trigger="hover" data-bs-placement="top" title="Remove">
+                                                        <a href=""
+                                                            class="text-danger d-inline-block remove-item-btn">
+                                                            <i class="ri-delete-bin-5-fill fs-16"></i>
+                                                        </a>
+                                                    </li>
+                                                </ul>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <div class="noresult" style="display: none">
+                                    <div class="text-center">
+                                        <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
+                                            colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
+                                        </lord-icon>
+                                        <h5 class="mt-2">Sorry! No Result Found</h5>
+                                        <p class="text-muted mb-0">We've searched more than 150+ Complains We did not find any
+                                            complains for you search.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-end mt-3">
+                                <div class="pagination-wrap hstack gap-2">
+                                    <a class="page-item pagination-prev disabled" href="#">
+                                        Previous
+                                    </a>
+                                    <ul class="pagination listjs-pagination mb-0"></ul>
+                                    <a class="page-item pagination-next" href="#">
+                                        Next
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
 
+                    </div>
                 </div>
 
             </div>
