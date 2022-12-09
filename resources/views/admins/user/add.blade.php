d8363501f4f4156e6dc11639a54cc1c2e87000af
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2022-12-09 20:44:23 +0100

    crud for user

---
+++ resources/views/admins/user/add.blade.php
@@ -0,0 +1,87 @@
+<div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header bg-light p-3">
+            <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="close-modal"></button>
+        </div>
+        <form action="{{ url('admin/user/add') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="modal-body">
+                <input type="hidden" id="id-field" />
+
+                <div class="row gy-4 mb-3">
+                    <div class="col-md-6">
+                        <div>
+                            <label for="amount-field" class="form-label">First Name</label>
+                            <input type="text" name="firstname" id="amount-field" class="form-control" placeholder="First Name" required />
+                            @error('firstname')
+                                <p class="text-danger">{{$message}}</p>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div>
+                            <label for="amount-field" class="form-label">Last Name</label>
+                            <input type="text" name="lastname" id="amount-field" class="form-control" placeholder="Last Name" required />
+                            @error('lastname')
+                                <p class="text-danger">{{$message}}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <label for="customername-field" class="form-label">Email</label>
+                    <input type="email" name="email" id="customername-field" class="form-control" placeholder="Enter email" required />
+                    @error('email')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="customername-field" class="form-label">Phone</label>
+                    <input type="number" name="phone" id="customername-field" class="form-control" placeholder="Enter Phone" required />
+                    @error('phone')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="customername-field" class="form-label">Address</label>
+                    <input type="text" name="address" id="customername-field" class="form-control" placeholder="Enter Address" required />
+                    @error('address')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="customername-field" class="form-label">Gender</label>
+                    <select class="form-control" data-trigger name="gender" id="payment-field" required>
+                        <option value="">Select Gender</option>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                    @error('gender')
+                        <p class="text-danger">{{$message}}</p>
+                    @enderror
+                </div>
+
+                <div class="row gy-4 mb-3">
+                    <div class="col-md-6">
+                        <div>
+                            <label for="amount-field" class="form-label">City</label>
+                            <input type="text" name="city" id="amount-field" class="form-control" placeholder="City" required />
+                            @error('city')
+                                <p class="text-danger">{{$message}}</p>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="hstack gap-2 justify-content-end">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success" id="add-btn">Save User</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
--- resources/views/admins/user/index.blade.php
+++ resources/views/admins/user/index.blade.php
@@ -8,12 +8,12 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0">Users</h4>
+                    <h4 class="mb-sm-0">User</h4>
 
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript: void(0);">Users</a></li>
-                            <li class="breadcrumb-item active">Users</li>
+                            <li class="breadcrumb-item"><a href="javascript: void(0);">User</a></li>
+                            <li class="breadcrumb-item active">User</li>
                         </ol>
                     </div>
 
@@ -35,7 +35,9 @@
                             </div>
                             <div class="col-sm-auto ms-auto">
                                 <div class="hstack gap-2">
-                                    <a href="" class="btn btn-success add-btn"><i class="ri-add-line align-bottom me-1"></i>Add User</a>
+                                    <button type="button" class="btn btn-success add-btn" data-bs-toggle="modal" id="create-btn" data-bs-target="#showModal">
+                                        <i class="ri-add-line align-bottom me-1"></i>Add User
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -62,7 +64,7 @@
                                         </tr>
                                     </thead>
                                     <tbody class="list form-check-all">
-                                        @foreach ($user as $users)
+                                        @foreach ($users as $user)
                                             <tr>
                                                 <th scope="row">
                                                     <div class="form-check">
@@ -70,34 +72,44 @@
                                                     </div>
                                                 </th>
                                                 <td class="id" style="display:none;"><a href="javascript:void(0);" class="fw-medium link-primary">#VZ2101</a></td>
-                                                <td class="name">{{ $users->fullname() }}</td>
-                                                <td class="leads_score">{{ $users->email }}</td>
-                                                <td class="phone">{{ $users->phone }}</td>
-                                                @if ($users->deleted_at == Null)
+                                                <td class="name">{{ $user->fullname() }}</td>
+                                                <td class="leads_score">{{ $user->email }}</td>
+                                                <td class="phone">{{ $user->phone }}</td>
+                                                @if ($user->deleted_at == Null)
                                                     <td class="status"><span class="badge badge-soft-success text-uppercase">Active</span></td>
-                                                @elseif ($users->deleted_at == !Null)
+                                                @elseif ($user->deleted_at == !Null)
                                                     <td class="status"><span class="badge badge-soft-danger text-uppercase">Block</span></td>
                                                 @endif
-                                                <td class="date left">{{ $users->created_at }}</td>
+                                                <td class="date left">{{ $user->created_at }}</td>
                                                 <td>
                                                     <ul class="list-inline hstack gap-2 mb-0">
-                                                        <li class="list-inline-item edit" data-bs-toggle="tooltip"
-                                                            data-bs-trigger="hover" data-bs-placement="top" title="Edit">
-                                                            <a href="{{ url('admin/user/'. $users->id) }}"
-                                                                class="text-primary d-inline-block edit-item-btn">
+                                                        <li class="list-inline-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="View">
+                                                            <a href="{{ url('admin/user/'. $user->id) }}" class="text-primary d-inline-block">
+                                                                <i class="ri-eye-fill fs-16"></i>
+                                                            </a>
+                                                        </li>
+                                                        <li class="list-inline-item edit" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Edit">
+                                                            <a href="#showModal{{ $user->id }}" data-bs-toggle="modal" class="text-primary d-inline-block edit-item-btn">
                                                                 <i class="ri-pencil-fill fs-16"></i>
                                                             </a>
                                                         </li>
-                                                        <li class="list-inline-item remove" data-bs-toggle="tooltip"
-                                                            data-bs-trigger="hover" data-bs-placement="top" title="Remove">
-                                                            <a href="{{ url('admin/user/'. $users->id.'/block') }}"
-                                                                class="text-danger d-inline-block remove-item-btn">
+                                                        <li class="list-inline-item" data-bs-toggle="tooltip"
+                                                            data-bs-trigger="hover" data-bs-placement="top"
+                                                            title="Remove">
+                                                            <a class="text-danger d-inline-block remove-item-btn"
+                                                                data-bs-toggle="modal" href="#deleteOrder{{ $user->id }}">
                                                                 <i class="ri-delete-bin-5-fill fs-16"></i>
                                                             </a>
                                                         </li>
                                                     </ul>
                                                 </td>
                                             </tr>
+                                            <div class="modal fade flip" id="deleteOrder{{ $user->id }}" tabindex="-1" aria-hidden="true">
+                                                @include('admins.user.delete')
+                                            </div>
+                                            <div class="modal fade zoomIn" id="showModal{{ $user->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                @include('admins.user.update')
+                                            </div>
                                         @endforeach
                                     </tbody>
                                 </table>
@@ -125,26 +137,8 @@
                             </div>
                         </div>
 
-                        <div class="modal fade zoomIn" id="deleteRecordModal" tabindex="-1" aria-labelledby="deleteRecordLabel"
-                            aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="btn-close"></button>
-                                    </div>
-                                    <div class="modal-body p-5 text-center">
-                                        <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop" colors="primary:#405189,secondary:#f06548" style="width:90px;height:90px"></lord-icon>
-                                        <div class="mt-4 text-center">
-                                            <h4 class="fs-semibold">You are about to delete a lead ?</h4>
-                                            <p class="text-muted fs-14 mb-4 pt-1">Deleting your lead will remove all of your information from our database.</p>
-                                            <div class="hstack gap-2 justify-content-center remove">
-                                                <button class="btn btn-link link-success fw-medium text-decoration-none" data-bs-dismiss="modal"><i class="ri-close-line me-1 align-middle"></i> Close</button>
-                                                <button class="btn btn-danger" id="delete-record">Yes, Delete It!!</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="modal fade" id="showModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            @include('admins.user.add')
                         </div>
 
                     </div>
