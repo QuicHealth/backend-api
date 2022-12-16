62aa2a75e2771c333d740efc79d4a727588ca4c1
officialozioma <dikep15@gmail.com>
2022-12-16 09:43:53 +0100

    Work in progress

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = parse_url(getenv('DATABASE_URL'));
+//$DATABASE_URL = parse_url(getenv('DATABASE_URL'));
 
 return [
 
@@ -67,34 +67,34 @@ return [
             ]) : [],
         ],
 
-        // 'pgsql' => [
-        //     'driver' => 'pgsql',
-        //     'url' => env('DATABASE_URL'),
-        //     'host' => env('DB_HOST', '127.0.0.1'),
-        //     'port' => env('DB_PORT', '5432'),
-        //     'database' => env('DB_DATABASE', 'forge'),
-        //     'username' => env('DB_USERNAME', 'forge'),
-        //     'password' => env('DB_PASSWORD', ''),
-        //     'charset' => 'utf8',
-        //     'prefix' => '',
-        //     'prefix_indexes' => true,
-        //     'schema' => 'public',
-        //     'sslmode' => 'prefer',
-        // ],
+         'pgsql' => [
+             'driver' => 'pgsql',
+             'url' => env('DATABASE_URL'),
+             'host' => env('DB_HOST', '127.0.0.1'),
+             'port' => env('DB_PORT', '5432'),
+             'database' => env('DB_DATABASE', 'forge'),
+             'username' => env('DB_USERNAME', 'forge'),
+             'password' => env('DB_PASSWORD', ''),
+             'charset' => 'utf8',
+             'prefix' => '',
+             'prefix_indexes' => true,
+             'schema' => 'public',
+             'sslmode' => 'prefer',
+         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST') ?? $DATABASE_URL['host'],
-            'port' => env('DB_PORT') ?? $DATABASE_URL['port'],
-            'database' =>
-            env('DB_DATABASE') ?? ltrim($DATABASE_URL['path'], '/'),
-            'username' => env('DB_USERNAME') ?? $DATABASE_URL['user'],
-            'password' => env('DB_PASSWORD') ?? $DATABASE_URL['pass'],
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'require',
-        ],
+//        'pgsql' => [
+//            'driver' => 'pgsql',
+//            'host' => env('DB_HOST') ?? $DATABASE_URL['host'],
+//            'port' => env('DB_PORT') ?? $DATABASE_URL['port'],
+//            'database' =>
+//            env('DB_DATABASE') ?? ltrim($DATABASE_URL['path'], '/'),
+//            'username' => env('DB_USERNAME') ?? $DATABASE_URL['user'],
+//            'password' => env('DB_PASSWORD') ?? $DATABASE_URL['pass'],
+//            'charset' => 'utf8',
+//            'prefix' => '',
+//            'schema' => 'public',
+//            'sslmode' => 'require',
+//        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
